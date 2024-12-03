3c913da1d54cf3e8aac095030c1972868d01abce
Joako <87424697+Nadharia@users.noreply.github.com>
2024-12-03 19:41:30 -0300

    practica 3-12

--- inicio.js
+++ inicio.js
@@ -1,3 +1,23 @@
-let numero=prompt("ingrese un numero con coma");
-numeroRedondeado=Math.round(numero);
-console.log(numeroRedondeado)
+
+
+function programa() {
+    let nombre=prompt("ingrese el nombre");
+    let ciudad=prompt("ingrese la ciudad");
+    let edad
+
+    do {
+        edad=prompt("ingrese la edad");
+        if (edad>=18) {
+            document.write("Hola "+nombre+", eres bienvenido desde "+ ciudad);
+            edad=0;
+        }else{
+            document.write("Hola "+nombre+", como eres menor de edad, no puedes acceder a este sitio.");
+            edad=0;
+        }
+
+    } while (edad!=0);
+
+
+}
+
+
---
+++ tipodedatos.js
@@ -0,0 +1,4 @@
+let numero= 10;
+
+console.log(numero.toString(numero));
+
